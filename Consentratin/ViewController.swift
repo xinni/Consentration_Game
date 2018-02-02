75984249cacfbd1714eb4f89812280462a09f6ea
xinni <xinni@buffalo.edu>
2018-02-02 17:03:54 -0500

    Initial Commit

---
+++ Consentratin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Consentratin
+//
+//  Created by Xin Ni on 2/2/18.
+//  Copyright © 2018 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
